8b3552d4cc6debd664e0516dc19630b9a519b5f9
WanshanLi <wanshanl@andrew.cmu.edu>
2021-10-25 18:32:29 -0400

    update scripts

--- topic_SCORE.py
+++ topic_SCORE.py
@@ -12,7 +12,7 @@ def topic_score(word_doc, K_topic, K_cand, n_cluster, word_freq_quantile = 1, km
 
     word_doc = (word_doc.T / word_freq_trunk).T
     svd = scipy.sparse.linalg.svds(word_doc, k = K_topic)
-    Xi = svd.u
+    Xi = svd[0]
 
     # Step 1
     Xi[:,0] = abs(Xi[:,0])
@@ -20,13 +20,13 @@ def topic_score(word_doc, K_topic, K_cand, n_cluster, word_freq_quantile = 1, km
 
     # Step 2
     vertices = vertices_estimate(R, K_cand, n_cluster, kmeans_start = kmeans_start)
-    V = vertices.V
-    theta = vertices.theta
+    V = vertices[0]
+    theta = vertices[1]
 
     # Step 3
-    R_extend = np.concatenate((np.ones(n,1), R), axis = 1)
-    V_extend = np.concatenate((np.ones(n,1), V), axis =1)
-    Pi = R_extend @ np.linalg.solve(V_extend)
+    R_extend = np.concatenate((np.ones((n,1)), R), axis = 1)
+    V_extend = np.concatenate((np.ones((K_topic,1)), V), axis = 1)
+    Pi = R_extend @ np.linalg.inv(V_extend, )
     Pi = np.maximum(Pi, 0)
     rowsum = Pi.sum(axis = 1)
     Pi = Pi.T / rowsum
@@ -38,7 +38,7 @@ def topic_score(word_doc, K_topic, K_cand, n_cluster, word_freq_quantile = 1, km
     colsum = A_hat.sum(axis = 0)
     A_hat = A_hat / colsum
 
-    return A_hat, R, V, Pi
+    return A_hat, R, V, Pi, theta
 
 
 def vertices_estimate(R, K_cand, n_cluster, kmeans_start):
@@ -59,7 +59,7 @@ def vertices_estimate(R, K_cand, n_cluster, kmeans_start):
     top2 = np.array([top2[0][0], top2[1][0]])
     
     theta0 = theta[top2,:]
-    theta = theta[np.setdiff1d(np.arange(n),top2),:]
+    theta = theta[np.setdiff1d(np.arange(n_cluster),top2),:]
 
     if K_cand > 2:
         for k0 in range(3, K_cand + 1):
@@ -70,12 +70,12 @@ def vertices_estimate(R, K_cand, n_cluster, kmeans_start):
             avg_dist = distance.mean(axis = 0)
             max_ix = np.argmax(avg_dist)
             
-            theta0 = np.concatenate((theta0, theta[max_ix,:]), axis = 0)
+            theta0 = np.concatenate((theta0, theta[max_ix:max_ix + 1,:]), axis = 0)
             theta = theta[np.setdiff1d(np.arange(n_k0),max_ix),:]
         theta = theta0
 
     # Step 2b
-    index_matrix = np.array(list(itertools.combinations(np.arange(K_cand),K))) # K_topic x (K_cand choose K_topic) array
+    index_matrix = np.array(list(itertools.combinations(np.arange(K_cand),K))) # (K_cand choose K_topic) X K_topic array
     max_outlier = np.zeros(index_matrix.shape[0])
     for i in range(index_matrix.shape[0]):
         for j in range(K_cand):
@@ -88,15 +88,15 @@ def vertices_estimate(R, K_cand, n_cluster, kmeans_start):
 def simplex_distance(x, V):
     n_v, p = V.shape
     
-    G = np.concatenate((np.eye(n_v - 1), -np.ones(n_v - 1)), axis = 1)
+    G = np.concatenate((np.eye(n_v - 1), -np.ones((n_v - 1, 1))), axis = 1)
     VV = G @ V
     P = VV @ VV.T
-    q = VV @ (x - V[nv - 1, :]).reshape((p,1))
+    q = VV @ (x - V[n_v - 1, :]).reshape((p,1))
     h = np.zeros(n_v)
-    h[nv - 1] = -1
+    h[n_v - 1] = -1
 
-    obj = qp_const(P = P, q = q, G = G, h = h)
-    return np.sum((x - V[nv-1,:])**2) + 2 * obj
+    obj = qp_const(P = P, q = q, G = G.T, h = h)
+    return np.sum((x - V[n_v - 1,:])**2) + 2 * obj
 
     
 def qp_const(P,q,E = None,f = None,G = None,h = None,obj_only = True):
@@ -122,15 +122,22 @@ def qp_const(P,q,E = None,f = None,G = None,h = None,obj_only = True):
         args.extend([cvxopt.matrix(E), cvxopt.matrix(f)])
         
     cvxopt.solvers.options['show_progress'] = False
-    sol = cvxopt.solvers.qp(*args)
-    if 'optimal' not in sol['status']:
-        return None
-#     return solution
-    x = np.array(sol['x']).reshape((P.shape[1],))
-    obj = np.array(sol['y'])[0,0]
+    try:
+        sol = cvxopt.solvers.qp(*args)
+        if 'optimal' not in sol['status'] or not sol['y']:
+            # not sure if this is the best way to handle all non-optimal cases
+            obj = np.nan
+            x = None
+        else:
+            x = np.array(sol['x']).reshape((P.shape[1],))
+        #     obj = np.array(sol['y'])[0,0]
+            obj = sol['y'][0]
+    except:
+        # e.g., P is not positive-semidefinite
+        obj = np.nan
+        x = None
     if obj_only:
         result = obj
     else:
         result = [x,obj]
     return result
-
